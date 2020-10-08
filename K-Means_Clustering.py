1210c03d3b6351c8949937ee13a1f814b834a9b1
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-08 12:42:36 -0600

    Templates set up

--- K-Means_Clustering.py
+++ K-Means_Clustering.py
@@ -1,3 +1,16 @@
 # K-Means Clustering ALgorithm tutorial from Machine Learning A-Z - SuperDataScience
 # Input by Ryan L Buchanan 06OCT20
 
+# Import libraries 
+
+
+# Import dataset
+
+
+# Use the Elbow Method to find the optimal number of clusters
+
+
+# Train the K-Means model on the dataset
+
+
+# Visualize the clusters
